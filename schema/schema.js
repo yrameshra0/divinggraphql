9a81cd42ae97e39ce87c896e05ce7dd71ec100d3
Y Ramesh Rao <yrameshrao@gmail.com>
2017-05-01 12:45:10 +0530

    Associated nested data linked with user with graphql fetching the same

--- schema/schema.js
+++ schema/schema.js
@@ -1,22 +1,35 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
+
 const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
     GraphQLSchema
 } = graphql;
-const users = [
-    {id: '23', firstName: 'Bill', age: 20},
-    {id: '40', firstName: 'Samantha', age: 21}
-];
+
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: {
+        id: {type: GraphQLString},
+        name: {type: GraphQLString},
+        description: {type: GraphQLString}
+    }
+});
 
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
         id: {type: GraphQLString},
         firstName: {type: GraphQLString},
-        age: {type: GraphQLInt}
+        age: {type: GraphQLInt},
+        company: {
+            type: CompanyType,
+            resolve(parentValue, args){
+                return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+                    .then(response => response.data);
+            }
+        }
     }
 });
 
@@ -27,7 +40,8 @@ const RootQuery = new GraphQLObjectType({
             type: UserType,
             args: {id: {type: GraphQLString}},
             resolve(parentValue, args){
-                return _.find(users, {id: args.id});
+                return axios.get(`http://localhost:3000/users/${args.id}`)
+                    .then(response => response.data);
             }
         }
     }
