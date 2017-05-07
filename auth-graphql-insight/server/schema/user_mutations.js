efe980eb0974b87c6a9b30041ad6e7334efcdc8c
Y Ramesh Rao <yrameshrao@gmail.com>
2017-05-07 07:21:13 +0530

    Login, logout and root graphql query chucked in

--- auth-graphql-insight/server/schema/user_mutations.js
+++ auth-graphql-insight/server/schema/user_mutations.js
@@ -19,6 +19,25 @@ const mutation = new GraphQLObjectType({
             resolve(parentValue, {email, password}, req){
                 return userService.signup({email, password, req});
             }
+        },
+        login: {
+            type: UserType,
+            args: {
+                email: {type: new GraphQLNonNull(GraphQLString)},
+                password: {type: new GraphQLNonNull(GraphQLString)}
+            },
+            resolve(parentValue, {email, password}, req){
+                return userService.login({email, password, req})
+            }
+        },
+        logout: {
+            type: UserType,
+            resolve(parentValue, args, req){
+                const {user} = req;
+                req.logOut();
+
+                return user;
+            }
         }
     }
 });
