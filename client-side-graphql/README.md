12d9114af04655d4f3ffa3a79fd58d4e7d3b1466
Y Ramesh Rao <yrameshrao@gmail.com>
2017-06-12 05:32:15 +0530

    Initial commit with skeleton project
    GraphQL client side understanding related project skeleton in place.

---
+++ client-side-graphql/README.md
@@ -0,0 +1,3 @@
+# More Client Side GraphQL Explorations
+Experimenting with GraphQL over client side to get head around
+it.
---
+++ client-side-graphql/client/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import {ApolloProvider} from 'react-apollo';
+
+const client = new ApolloClient({});
+const Root = () => {
+    return (
+        <ApolloProvider client={client}>
+            <div>Lyrical</div>
+        </ApolloProvider>
+    );
+};
+
+ReactDOM.render(
+    <Root />,
+    document.querySelector('#root')
+);
